a958ac7de7ab717d413f75565b9f6aa702282cdb
AT8Cool <leo8gaming@gmail.com>
2025-06-29 17:22:50 +0530

    Minor update to requirements.txt

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-class User_data(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(150),nullable=False,unique=True)
-    password = db.Column(db.String(900),nullable = False)
